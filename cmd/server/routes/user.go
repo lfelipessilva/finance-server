be0807f27835b89b5808157b4340dd93895cf36f
Luís Felipe <luis.felipeszsv@gmail.com>
2025-07-28 21:12:09 -0300

    fix: api docs

--- cmd/server/routes/user.go
+++ cmd/server/routes/user.go
@@ -1,28 +1,21 @@
 package routes
 
 import (
-	"finance/config"
-	"finance/internal/handler/http"
-	authUC "finance/internal/usecase/auth"
-
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-
-	respository "finance/internal/repository/user"
-	usecase "finance/internal/usecase/user"
 )
 
 func UserRoutes(router *gin.RouterGroup, db *gorm.DB) {
-	cfg, err := config.Load()
-	if err != nil {
-		panic("failed to load config: " + err.Error())
-	}
+	// cfg, err := config.Load()
+	// if err != nil {
+	// 	panic("failed to load config: " + err.Error())
+	// }
 
-	userRepository := respository.NewPostgresRepository(db)
-	userUseCase := usecase.NewUserUseCse(userRepository)
-	authUseCase := authUC.NewAuthUseCase(cfg.JWTSecret, cfg.GoogleOAuthClientID, userUseCase)
-	userHandler := http.NewUserHandler(userUseCase, authUseCase)
+	// userRepository := respository.NewPostgresRepository(db)
+	// userUseCase := usecase.NewUserUseCse(userRepository)
+	// authUseCase := authUC.NewAuthUseCase(cfg.JWTSecret, cfg.GoogleOAuthClientID, userUseCase)
+	// userHandler := http.NewUserHandler(userUseCase, authUseCase)
 
-	router.GET("/user", userHandler.GetUsers)
-	router.POST("/user", userHandler.CreateUser)
+	// router.GET("/user", userHandler.GetUsers)
+	// router.POST("/user", userHandler.CreateUser)
 }
